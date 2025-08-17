92a785519e25aaa080945d59e132c4a776f79f80
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-17 19:07:05 +0530

    initial route add routes add

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:age_calculator/routes/const_routes.dart';
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
 
@@ -8,11 +9,16 @@ void main(){
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      home: SplashScreen(),
+      initialRoute: kSplashScreen,
+      routes: {
+        // TODO: add routes here
+        kSplashScreen : (context) => SplashScreen();
+      },
     );
   }
 }
