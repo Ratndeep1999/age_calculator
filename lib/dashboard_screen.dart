9e768f257543b9d8941ca5022ae3312fc675d127
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-17 19:12:56 +0530

    dashboard screen add

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -10,6 +10,11 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Column(children: [
+
+      ],)),
+    );
   }
 }
