8d2d0bb0c4a9972ab94d2beb5e4dc73faa936e6b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-19 17:49:42 +0530

    birth_details_section.dart : old details section add and parameters also add and padding maintain of both axis

--- lib/Dashboard_Widgets/birth_details_section.dart
+++ lib/Dashboard_Widgets/birth_details_section.dart
@@ -9,7 +9,6 @@ class BirthDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
-      // height: 400,
       child: DecoratedBox(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
@@ -20,18 +19,18 @@ class BirthDetails extends StatelessWidget {
           boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 8.0)],
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
               Text(
                 'You are (You age right now)',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-              SizedBox(height: 15),
+              SizedBox(height: 12),
 
               /// Years, Months and Days Box
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   /// Years
                   YeMoDaBox(
@@ -56,11 +55,28 @@ class BirthDetails extends StatelessWidget {
                 ],
               ),
 
-              SizedBox(height: 15),
-              Divider(),
+              SizedBox(height: 20),
+              Divider(thickness: 1.3,),
+              SizedBox(height: 15,),
 
               /// Olds Details Section
-              OldsDetails(),
+              // months
+              OldsDetails(old: 'Months old', value: '233',),
+              SizedBox(height: 20,),
+              // weeks
+              OldsDetails(old: 'Weeks old', value: '1020',),
+              SizedBox(height: 20,),
+              // days
+              OldsDetails(old: 'Days old', value: '7039',),
+              SizedBox(height: 20,),
+              // hours
+              OldsDetails(old: 'Hours old(approx)', value: '168555',),
+              SizedBox(height: 20,),
+              // minutes
+              OldsDetails(old: 'Minutes old(approx)', value: '10112273',),
+              SizedBox(height: 20,),
+              // seconds
+              OldsDetails(old: 'Seconds old(approx)', value: '606735377',),
             ],
           ),
         ),
