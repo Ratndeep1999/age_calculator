14bd05fbb9e54249f1d425639736b84cbadb6dd0
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-19 16:59:36 +0530

    birth_details_section.dart : Widget refactored and years, months and days also refactor

--- lib/Dashboard_Widgets/birth_details_section.dart
+++ lib/Dashboard_Widgets/birth_details_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Birth_Details_Widgets/ye_mo_da_section.dart';
+
 class BirthDetails extends StatelessWidget {
   const BirthDetails({super.key});
 
@@ -28,43 +30,11 @@ class BirthDetails extends StatelessWidget {
               Row(
                 children: [
                   /// Years
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.08,
-                        width: MediaQuery.of(context).size.width * 0.18,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Color(0xFF6967db),
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '19',
-                              style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 8),
-
-                      /// Label text
-                      Text(
-                        'Years',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black45,
-                        ),
-                      ),
-                    ],
-                  ),
+                  YeMoDaBox(),
                   /// Months
+                  YeMoDaBox(),
                   /// Days
+                  YeMoDaBox(),
                 ],
               ),
             ],
@@ -74,3 +44,5 @@ class BirthDetails extends StatelessWidget {
     );
   }
 }
+
+
