7f9b7c2984e155274805b2e384d0f4cf25577457
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-19 16:54:19 +0530

    dashboard_screen.dart : Birth Details section add and refactor

---
+++ lib/Dashboard_Widgets/birth_details_section.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class BirthDetails extends StatelessWidget {
+  const BirthDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 200,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(8),
+            bottomLeft: Radius.circular(8),
+          ),
+          color: Color(0xFFffffff),
+          boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 8.0)],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('You are (You age right now)'),
+              SizedBox(height: 15),
+
+              /// Years, Months and Days Box
+              Row(
+                children: [
+                  /// Years
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        width: MediaQuery.of(context).size.width * 0.18,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Color(0xFF6967db),
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '19',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+
+                      /// Label text
+                      Text(
+                        'Years',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                  /// Months
+                  /// Days
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
