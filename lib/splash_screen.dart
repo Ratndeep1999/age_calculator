5e0645a999fbc5cc8de937be755b558a1fab9080
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-20 18:13:55 +0530

    splash screen duration add as previous

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 0), () => navigateToNextScreen());
+    Future.delayed(Duration(seconds: 2), () => navigateToNextScreen());
     super.initState();
   }
   // method to navigate
