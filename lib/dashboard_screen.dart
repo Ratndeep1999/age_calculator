fdb8df08ddf49cd61472dddd6845a4d9d3f28340
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-20 15:07:35 +0530

    dashboard_screen.dart : method call, show calender and then Birthdate get

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'Dashboard_Widgets/birth_details_section.dart';
 import 'Dashboard_Widgets/birthdate_section.dart';
@@ -11,6 +14,17 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  DateTime selectedDate = DateTime.now();
+  List<String> weekDay = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +34,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
           child: Column(
             children: [
               /// Birthdate Section
-              Birthdate(),
+              Birthdate(
+                selectBirthdate: () {
+                  debugPrint('click calender icon');
+                  selectBirthDate();
+                },
+                day: selectedDate.day.toString(),
+                month: selectedDate.month.toString(),
+                year: selectedDate.year.toString(),
+                weekday: weekDay[selectedDate.weekday - 1],
+              ),
 
               /// Birth Details Section
               BirthDetails(),
@@ -33,6 +56,58 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ),
       ),
+    );
+  }
+
+  // method to show platform specific calender
+  void selectBirthDate() {
+    if (Platform.isAndroid) {
+      // show android calendar
+      showAndroidCalender();
+    } else if (Platform.isIOS) {
+      // show ios calender
+    } else if (kIsWeb) {
+      // show web calender
+    }
+  }
+
+  // method to show android calender
+  Future<void> showAndroidCalender() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900, 1, 1),
+      lastDate: DateTime.now(),
+    );
+
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+      });
+      debugPrint(selectedDate.toString());
+      debugPrint(weekDay[selectedDate.weekday - 1]);
+    }
+  }
+
+  // method to show ios calender
+  void showIosCalender() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 200,
+          child: CupertinoDatePicker(
+            maximumDate: DateTime.now(),
+            minimumDate: DateTime(DateTime.now().year - 100),
+            initialDateTime: selectedDate,
+            onDateTimeChanged: (DateTime nowDate) {
+              setState(() {
+                selectedDate = nowDate;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
